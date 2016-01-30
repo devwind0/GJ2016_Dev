91d2ca2ddafd1ee417db9a7b73d350f3c75b67b4
Leo <leonpard.li@utah.edu>
2016-01-29 23:52:48 -0700

    Leo: Add the script of Split screen.
    I am working on the FPS controller right now.

---
+++ GJ2016/Assets/Leo/LeoScripts/PlayerCam.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class PlayerCam : MonoBehaviour {
+
+    public enum Player
+    {
+        PlayerA,
+        PlayerB,
+    }
+    public  Player _player = Player.PlayerA;
+    Camera _cam = null;
+
+    // Use this for initialization
+    void Start () {
+        _cam = GetComponent<Camera>();
+        switch (_player)
+        {
+            case Player.PlayerA:
+                _cam.rect = new Rect(0f, 0f, 0.5f, 1f);
+                break;
+            case Player.PlayerB:
+                _cam.rect = new Rect(0.5f, 0f, 0.5f, 1f);
+                break; 
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
