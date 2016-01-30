2fd9cd7f89f62468b80dae99612b26d416a0815c
Leo <leonpard.li@utah.edu>
2016-01-30 15:22:26 -0700

    Leo: add the CameraPick functionality.

---
+++ GJ2016/Assets/Leo/LeoScripts/CameraPick.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
+public class CameraPick : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        /*
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Organ"))
+            {
+                Debug.Log("Orgn" + hit.collider.name);
+            }
+        }
+        */
+    }
+
+    void OnTriggerEnter(Collider i_other)
+    {
+        Organ organ = i_other.GetComponent<Organ>();
+        if (organ == null)
+            return;
+        organ.OnSelectStart();
+    }
+
+
+    void OnTriggerExit(Collider i_other)
+    {
+        Organ organ = i_other.GetComponent<Organ>();
+        if (organ == null)
+            return;
+        organ.OnSelectEnd();
+    }
+
+}
