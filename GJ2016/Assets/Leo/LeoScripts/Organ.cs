eb6c8f7ac6d5f9d33b5f346642f0337ef8a4f372
Leo <leonpard.li@utah.edu>
2016-01-30 12:16:46 -0700

    Leo:   the Terrain exists in LeoTestScene.Unity

---
+++ GJ2016/Assets/Leo/LeoScripts/Organ.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class Organ : MonoBehaviour {
+
+    public Material _hightLitMaterial;
+    MeshRenderer[] _renders = null;
+    List<Material> _defaultMats = new List<Material>();
+
+    public OrganType _organType = OrganType.Unkonw;
+
+    public bool OnSelected
+    {
+        get { return _onSelected; }
+    }
+    bool _onSelected = false;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+        _renders = GetComponentsInChildren<MeshRenderer>();
+        if (_renders == null)
+            return;
+        foreach (MeshRenderer mr in _renders)
+        {
+            foreach (Material mat in mr.materials)
+            {
+                _defaultMats.Add(mat);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void OnSelectStart()
+    {
+        if (_renders == null)
+            return;
+
+        foreach (MeshRenderer mr in _renders)
+        {
+            for (int i = 0; i < mr.materials.Length; ++i)
+            {
+                mr.materials[i] = _hightLitMaterial;
+               // mr.materials[i].color = Color.red;
+            }
+        }
+        _onSelected = true;
+    }
+
+    void OnSelectEnd()
+    {
+        if (_renders == null)
+            return;
+        int index = 0;
+        foreach (MeshRenderer mr in _renders)
+        {
+            for (int i = 0; i < mr.materials.Length; ++i)
+            {
+                mr.materials[i] = _defaultMats[index++];
+             //   mr.materials[i].color = Color.grey;
+            }
+        }
+        _onSelected = false;
+    }
+
+    void OnTriggerEnter(Collider i_other)
+    {
+        Debug.Log("Entered");
+        OnSelectStart();
+    }
+
+    void OnTriggerExit(Collider i_other)
+    {
+        Debug.Log("Exited");
+        OnSelectEnd();
+    }
+}
---
+++ GJ2016/Assets/Leo/LeoScripts/OrganSlots.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum OrganType
+{
+    Kidney,
+    Heart,
+    Lung,
+    Brain,
+    Unkonw,
+}
+
+public class OrganSlots : MonoBehaviour {
+    [SerializeField]
+    Organ[] _organs;
+
+    [SerializeField]
+    GameObject[] _slots;
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        if (_organs == null)
+            return;
+        if (Input.GetButtonDown("Fire1")) {
+
+            OrganType selectedType = OrganType.Unkonw;
+            int i = 0;
+            for ( ; i< _organs.Length; ++i)
+            {
+                if (_organs[i].OnSelected)
+                {
+                    selectedType = _organs[i]._organType;
+                    break;
+                }
+            }
+            if (selectedType != OrganType.Unkonw)
+                ChangeOrder(i);
+        }
+	}
+
+    void ChangeOrder(int indexOfOrgan)
+    {
+        Debug.Log("OnFire:" + indexOfOrgan);
+        int nextOrganIndex = (indexOfOrgan + 1) % _organs.Length;
+        Vector3 temp = _organs[indexOfOrgan].transform.localPosition;
+        _organs[nextOrganIndex].transform.localPosition = _organs[indexOfOrgan].transform.localPosition;
+        _organs[indexOfOrgan].transform.localPosition = temp;
+    }
+
+}
