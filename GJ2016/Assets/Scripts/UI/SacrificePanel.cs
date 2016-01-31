af985103404dd61339b20cdbae0293ab3b6668a4
Xu, Miao <mxu@ea.com>
2016-01-31 15:45:21 -0700

    Final pass for the first build

--- GJ2016/Assets/Scripts/UI/SacrificePanel.cs
+++ GJ2016/Assets/Scripts/UI/SacrificePanel.cs
@@ -6,11 +6,13 @@ public class SacrificePanel : MonoBehaviour {
 
 	//My current player index.
 	public int Index { get; set; }
-
+	public int selectionIndex = 0;
 	public Button CutHandButton;
 	public Button CutLegButton;
 	public Button CutEyeButton;
 	public Button ToggleSacrificeButton;
+	public Transform Arrow;
+
 
 	public GameObject OptionPanel;
 	private bool isPanelActive = false;
@@ -25,6 +27,8 @@ public class SacrificePanel : MonoBehaviour {
 		CutEyeButton.onClick.AddListener(OnCutEyeButtonClicked);
 		CutLegButton.onClick.AddListener(OnCutLegButtonClicked);
 		OptionPanel.SetActive (isPanelActive);
+		Arrow.gameObject.SetActive (isPanelActive);
+		SetArrow (selectionIndex);
 	}
 
 	void Update()
@@ -34,6 +38,64 @@ public class SacrificePanel : MonoBehaviour {
 		{
 			isPanelActive = !isPanelActive;
 			OptionPanel.SetActive (isPanelActive);
+			Arrow.gameObject.SetActive (isPanelActive);
+		}
+
+		if (isPanelActive) 
+		{
+			if ((Input.GetButtonDown ("CarryOrgan-controller") && Index == 1)
+			    || (Input.GetButtonDown ("CarryOrgan-keyboard") && Index == 0)) 
+			{
+				switch (selectionIndex) 
+				{
+				case 0:
+					OnCutLegButtonClicked ();
+					break;
+				case 1:
+					OnCutHandButtonClicked ();
+					break;
+				case 2:
+					OnCutEyeButtonClicked ();
+					break;
+				}
+			}
+
+			if (Input.GetKeyDown (KeyCode.UpArrow) ||
+				Input.GetAxis ("Axis-controller") > 0) 
+			{
+				selectionIndex--;
+				if (selectionIndex < 0) {
+					selectionIndex = 2;
+				}
+				SetArrow (selectionIndex);
+			}
+
+			if (Input.GetKeyDown (KeyCode.DownArrow) ||
+				Input.GetAxis ("Axis-controller") < 0) 
+			{
+				selectionIndex++;
+				if (selectionIndex > 2) {
+					selectionIndex = 0;
+				}
+				SetArrow (selectionIndex);
+			}
+		}
+	}
+
+	private void SetArrow(int selectionIndex)
+	{
+		RectTransform rect = Arrow as RectTransform;
+		switch (selectionIndex) 
+		{
+		case 0:
+			rect.anchoredPosition3D = new Vector3 (-31.4f, -24f, 0);
+			break;
+		case 1:
+			rect.anchoredPosition3D = new Vector3 (-31.4f, -66.51f, 0);
+			break;
+		case 2:
+			rect.anchoredPosition3D = new Vector3 (-31.4f, -108.52f, 0);
+			break;
 		}
 	}
 
@@ -73,10 +135,12 @@ public class SacrificePanel : MonoBehaviour {
 	private void CoolDownButtons(){
 		isPanelActive = !isPanelActive;
 		OptionPanel.SetActive (isPanelActive);
+		Arrow.gameObject.SetActive (isPanelActive);
 	}
 
 	private void RecoverButtons(){
 		isPanelActive = !isPanelActive;
 		OptionPanel.SetActive (isPanelActive);
+		Arrow.gameObject.SetActive (isPanelActive);
 	}
 }
