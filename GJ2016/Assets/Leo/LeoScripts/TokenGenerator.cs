a8ade5870ade40090a9081918ee3192b1c7645d5
Leo <leonpard.li@utah.edu>
2016-01-31 13:56:42 -0700

    Leo: add a StateLock for the Doors of different puzzles.

---
+++ GJ2016/Assets/Leo/LeoScripts/TokenGenerator.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(BoxCollider))]
+public class TokenGenerator : MonoBehaviour {
+
+    BoxCollider _tokenTrigger = null;
+    public PlayerEnum _player = PlayerEnum.PlayerA;
+    public GameObject _tokenPrefab = null;
+    public Transform _tokenGenPos = null;
+    // Use this for initialization
+    void Start () {
+        _tokenTrigger = GetComponent<BoxCollider>();
+    }
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider i_other) {
+        CharacterController cc = i_other.GetComponent<CharacterController>();
+        if (cc == null)
+            return;
+        if (_tokenTrigger == null)
+            return;
+        int playerIndex = 0;
+        switch (_player)
+        {
+            case PlayerEnum.PlayerA:
+                playerIndex = 0;
+                break;
+            case PlayerEnum.PlayerB:
+                playerIndex = 1;
+                break;
+        }
+        Score playerScore = PlayerManager.Singleton.GetScore(playerIndex);
+        int tokenIndex = playerScore.GetIndexOfWhatsLeft();
+        Instantiate(_tokenPrefab, _tokenGenPos.position, Quaternion.identity);
+    }
+}
